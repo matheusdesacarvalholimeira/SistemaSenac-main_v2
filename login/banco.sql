5a867de36c8de22cea727583c5206cc1ea243b1c
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2023-05-12 10:06:00 -0300

    Update banco.sql

--- login/banco.sql
+++ login/banco.sql
@@ -12,7 +12,26 @@ CREATE TABLE users (
 
 create table questoes(
 id_ques int primary key auto_increment,
-questao1 char(1)
+questao1 char(1),
+questao2 char(1),
+questao3 char(1),
+questao4 char(1),
+questao5 char(1),
+questao6 char(1),
+questao7 char(1),
+questao8 char(1),
+questao9 char(1),
+questao10 char(1),
+questao11 char(1),
+questao12 char(1),
+questao13 char(1),
+questao14 char(1),
+questao15 char(1),
+questao16 char(1),
+questao17 char(1),
+questao18 char(1),
+questao19 char(1),
+questao20 char(1)
 );
 
 alter table questoes add column id int;
@@ -24,4 +43,5 @@ alter table questoes add constraint id foreign key(id) references users(id);
 drop database test;
 
 select * from users;
-select * from questoes
+select * from questoes;
+
